fbe27d96a21687a4544ce1a71861519cb422a02d
Sanja <sanja.pistac@gmail.com>
2023-03-03 20:15:21 +0100

    working on details...

--- JS/ui.js
+++ JS/ui.js
@@ -13,11 +13,11 @@ export let renderCharacters = characters => {
         
         <div class="card" id="${character.id}">
             
-          <img src="${character.image}" class="card-img-top mt-3 img-fluid rounded" alt="Character Image">
+          <img src="${character.image}" class="card-img-top img-fluid  mt-4 rounded" alt="Character Image">
             
-          <div class="card-body">
+          <div class="card-body d-flex flex-column justify-content-center align-items-center">
               <h3 class="card-title mt-3">${character.name}</h3>
-              <a href="#" class="btn border-2 border-success m-4"><i class="fa fa-thumbs-up"></i> Like</a>
+              <a href="#" class="btn border-5 border-success m-4 col-8"><i class="fa fa-thumbs-up"></i> Like</a>
           </div>
         
         </div>
@@ -41,19 +41,19 @@ export let renderSingleCharacter = character => {
         <div id="character-info" class="d-flex flex-row flex-wrap justify-content-around">
 
             <div class="col-md-7 rounded">
-                <img src=${character.image} class="card-img-top img-fluid img-thumbnail rounded-3" alt="Character Image">
+                <img src=${character.image} class="card-img-top img-fluid img-thumbnail rounded-3 m-5" alt="Character Image">
             </div>
             
-            <div class="col-md-4 rounded">
+            <div class="col-md-4">
                 
                 <ul class="p-1 word-wrap list-group img-thumbnail text-center">
 
-                    <li class="list-group-item"><h4>Status:</h4>${character.status}</li>
-                    <li class="list-group-item"><h4>Species:</h4>${character.species}</li>
-                    <li class="list-group-item"><h4>Gender:</h4>${character.gender}</li>
-                    <li class="list-group-item"><h4>Origin</h4>${character.origin}</li>
-                    <li class="list-group-item"><h4>Location:</h4>${character.location}</li>
-                    <li class="list-group-item"><h4>Episodes:</h4> ${character.episode}</li>
+                    <li class="list-group-item"><h4>Status:</h4><p>${character.status}</p></li>
+                    <li class="list-group-item"><h4>Species:</h4><p>${character.species}</p></li>
+                    <li class="list-group-item"><h4>Gender:</h4><p>${character.gender}</p></li>
+                    <li class="list-group-item"><h4>Origin:</h4><p>${character.origin}</p></li>
+                    <li class="list-group-item"><h4>Location:</h4><p>${character.location}</p></li>
+                    <li class="list-group-item"><h4>Episodes:</h4><p>${character.episode}</p></li>
                 
                 </ul>
                 
@@ -61,7 +61,11 @@ export let renderSingleCharacter = character => {
 
         </div>
 
-        
+        <div class="column d-flex justify-content-center m-5">
+            <a href="" class="btn go-back-button">Go Back to Home Page</a>
+        </div>
+
+
     </div
 
     
